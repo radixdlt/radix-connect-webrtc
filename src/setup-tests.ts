6d0d5e49cbc46fb40ac4439c4fc92fa5cd13af3f
Alex Stelea <alex.s@rdx.works>
2023-09-28 08:30:43 +0200

    fix: isomorphic crypto

--- src/setup-tests.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-// import { randomUUID, webcrypto } from 'node:crypto'
-// import { polyfillWebRTC } from './polyfill'
-
-// global.crypto.subtle = webcrypto.subtle
-// global.crypto.randomUUID = randomUUID
-
-// polyfillWebRTC()
