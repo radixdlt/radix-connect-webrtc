32e71e5e23be5b8d42c4a14bd31ae58d607bbf94
Alex Stelea <alex.s@rdx.works>
2023-11-28 16:55:40 +0100

    fix: add negotiation timeout

--- src/connector/webrtc/webrtc-client.ts
+++ src/connector/webrtc/webrtc-client.ts
@@ -9,6 +9,7 @@ import {
   concatMap,
   filter,
   first,
+  map,
   merge,
   mergeMap,
   Subscription,
@@ -49,6 +50,7 @@ export const WebRtcClient = (input: {
   confirmationTimeout: number
   restart: () => void
   dependencies: Dependencies
+  negotiationTimeout: number
 }) => {
   const logger = input.logger
   const subjects = input.subjects
@@ -96,6 +98,27 @@ export const WebRtcClient = (input: {
   const onLocalAnswer$ = subjects.answerSubject
 
   const subscriptions = new Subscription()
+
+  subscriptions.add(
+    subjects.offerSubject
+      .pipe(
+        switchMap(() =>
+          merge(
+            subjects.dataChannelStatusSubject.pipe(
+              filter((status) => status === 'open'),
+              map(() => false),
+            ),
+            timer(5_000).pipe(map(() => true)),
+          ).pipe(
+            first(),
+            tap((shouldRestart) => {
+              if (shouldRestart) input.restart()
+            }),
+          ),
+        ),
+      )
+      .subscribe(),
+  )
 
   subscriptions.add(
     signalingClient.remoteClientConnected$
